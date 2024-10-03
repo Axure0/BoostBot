c92a3cda83ef430c6efde3d2c00b79a1f977d3e5
Axure <baldarzana7@gmail.com>
2024-10-03 16:35:43 +0100

    commit #37

--- commands/utils/check-token.js
+++ commands/utils/check-token.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('discord.js')
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js')
 
 const { captchaKey } = require('../../config.json')
 
@@ -10,7 +10,7 @@ const { Client } = require('discord.js-selfbot-v13');
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('check-token')
-    .setDescription("Checks a token's boosts.")
+    .setDescription("Checks a token's details.")
     .addStringOption(option => 
         option.setName("token")
             .setDescription("The token to check.")
@@ -33,9 +33,51 @@ module.exports = {
     });
 
     client.on('ready', async () => {
-        const allBoosts = await client.billing.fetchGuildBoosts()
+        const boosts = await client.billing.fetchGuildBoosts()
+        const subscription = await client.billing.fetchCurrentSubscription()
 
-        return interaction.reply({ content: `Token has \`${allBoosts.size}\` boosts.`, ephemeral: true })
+        const premiumObj = {
+          "0": "No Nitro",
+          "1": "Nitro Classic",
+          "2": "Nitro",
+          "3": "Nitro Basic"
+        }
+        
+        const embed = new EmbedBuilder()
+        .setTitle(`${client.user.tag}`)
+        .setDescription(`${client.user.bio}`)
+        .setColor(client.user.accentColor)
+        .setFields(
+          { name: "2FA Enabled", value: `${client.user.mfaEnabled}`, inline: true },
+          { name: "Verified", value: `${client.user.verified}`, inline: true },
+          { name: "Phone Number", value: `${client.user.phone}`, inline: true },
+          { name: "Boosts", value: `${boosts.size}`, inline: true },
+          { name: "Nitro", value: `${premiumObj[String(client.user.premiumType)]}`, inline: true }
+        )
+        .setThumbnail(client.user.avatarURL())
+
+        if(bool === true) {
+          let startedt = Date.now()
+          let endst = Date.now()
+
+          subscription.map((x) => {
+            const [y, m, d] = x.current_peroid_start.split("-")
+            const ts1 = new Date(`${d} ${m} ${y}`)
+
+            startedt = ts1.getTime()
+
+            const [y1, m1, d1] = x.current_period_end.split("-")
+            const ts2 = new Date(`${d1} ${m1} ${y1}`)
+
+            endst = ts2.getTime()
+          })
+          embed.addFields(
+            { name: "Nitro Started", value: `<t:${startedt}>`, inline: true },
+            { name: "Nitro Ends", value: `<t:${endst}>`, inline: true }
+          )
+        }
+
+        return interaction.reply({ embeds: [embed], ephemeral: true })
     })
 
     await client.login(token)
